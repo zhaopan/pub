6ddf44ff0d8a4f6143ef0960382cb80b67bb2d8c
zhaopan <syszhaopan@gmail.com>
2018-05-23 15:33:11 +0800

    docs(log4net): 添加 log4net 备注 相关文档

---
+++ application/log4net/CustomMSSQLAppender.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data;
+using log4net.Appender;
+
+namespace System
+{
+    public class CustomMSSQLAppender : AdoNetAppender
+    {
+        protected override IDbConnection CreateConnection(Type connectionType, string connectionString)
+        {
+            var connStr = EncryptTools.Decrypt(connectionString);
+            return base.CreateConnection(connectionType, connStr);
+        }
+    }
+}
---
+++ application/log4net/Global.asax.cs
@@ -0,0 +1,5 @@
+﻿private void Application_Start(object sender, EventArgs e)
+{
+    log4net.Config.XmlConfigurator.Configure();
+    LogOuts.Info("Service Started");
+}
