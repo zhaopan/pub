91cbfb8fa4a711ff4bee2e3f65bbe6d8ec869e09
zhaopan <syszhaopan@gmail.com>
2019-01-11 15:29:50 +0800

    docs(docker): 添加 Docker 相关文档

---
+++ docker/init/init.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+##
+## mkdir some container configs
+##
+mkdir -p ~/nginx
+mkdir -p ~/nginx/html
+mkdir -p ~/mysql
+mkdir -p ~/mysql/data
+mkdir -p ~/gogs
+
+##
+## cp tmp-container configs
+##
+
+# nginx
+docker cp tmpnginx:/etc/nginx/nginx.conf ~/nginx/nginx.conf
+docker cp tmpnginx:/etc/nginx/conf.d ~/nginx
+
+# mysql
+docker cp tmpmysql:/etc/mysql/my.cnf ~/mysql/my.cnf
+docker cp tmpmysql:/etc/mysql/conf.d ~/mysql
+
+# drop tmp-container
+docker rm -f tmpmysql tmpnginx
