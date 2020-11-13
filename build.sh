c38a6bb1a6c695719f9ec9922c06f40d84469d90
zpx <zpx@zpxdeiMac.local>
2020-11-14 02:14:58 +0800

    update build.sh To README.sh

--- build.sh
+++ build.sh
@@ -11,16 +11,37 @@ getdir(){
         DIR_OR_FILE=$1"/"$ELEMENT
         if [ -d $DIR_OR_FILE ]
         then
-            echo "- ["$ELEMENT"]("$URL"/tree/master/"$DIR_OR_FILE")"
-            COUNT=`expr $COUNT + 1`
+            echo "${SPACE_STR:0:$COUNT}- ["$ELEMENT"]("$URL"/tree/master/"$DIR_OR_FILE")" >> README.md
+            COUNT=`expr $COUNT + 2`
             getdir $DIR_OR_FILE $COUNT
-            COUNT=1
+            COUNT=0
+            #echo $COUNT
+            #echo ""
         else
-            echo "  - ["$ELEMENT"]("$URL"/blob/master/"$ELEMENT")"
+            echo "${SPACE_STR:0:$COUNT}- ["$ELEMENT"]("$URL"/blob/master/"$DIR_OR_FILE")" >> README.md
         fi
     done
 }
 
+SPACE_STR="                                          "
 ROOT_DIR="."
 URL='https://github.com/zhaopan/codesnippet'
-getdir $ROOT_DIR 1
+
+
+tee README.md <<-'EOF'
+# 个人代码片段整理
+
+这些代码片段都是以前日常记录的，用于备忘和方便查询，防老年痴呆。
+
+<!-- TOC -->
+
+EOF
+
+echo 'Append document to readme.md'
+
+getdir $ROOT_DIR 0
+
+tee -a README.md <<-'EOF'
+
+<!-- /TOC -->
+EOF
