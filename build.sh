b1f71ac414a036692a2c7e7362d2e621bd4b4cf6
zhaopan <syszhaopan@gmail.com>
2023-03-03 17:30:00 +0800

    fix(build.sh): fix url

--- build.sh
+++ build.sh
@@ -1,30 +1,5 @@
 #!/bin/bash
 
-
-getdir_old(){
-    COUNT=1
-    if [ $COUNT -ne $2 ]
-    then
-        COUNT=$2
-    fi
-    for ELEMENT in `ls $1`
-    do
-        DIR_OR_FILE=$1"/"$ELEMENT
-        if [ -d $DIR_OR_FILE ]
-        then
-            echo "${SPACE_STR:0:$COUNT}- ["$ELEMENT"]("$URL"/tree/master/"${DIR_OR_FILE/.\//}")" >> README.md
-            COUNT=`expr $COUNT + 2`
-            getdir $DIR_OR_FILE $COUNT
-            COUNT=$2
-        else
-            if [[ $DIR_OR_FILE == *.md ]] # 过滤非 *.md 文档
-            then
-                echo "${SPACE_STR:0:$COUNT}- ["$ELEMENT"]("$URL"/blob/master/"${DIR_OR_FILE/.\//}")" >> README.md
-            fi
-        fi
-    done
-}
-
 getdir(){
     COUNT=1
     if [ $COUNT -ne $2 ]
@@ -37,7 +12,6 @@ getdir(){
         if [ -d $DIR_OR_FILE ]
         then
             echo "${SPACE_STR:0:$COUNT}- ["$ELEMENT"]("${DIR_OR_FILE/.\//}")" >> README.md
-            #echo $DIR_OR_FILE
             COUNT=`expr $COUNT + 2`
             getdir $DIR_OR_FILE $COUNT
             COUNT=$2
@@ -52,7 +26,6 @@ getdir(){
 
 SPACE_STR="                                          "
 ROOT_DIR="."
-URL='https://github.com/zhaopan/pub'
 
 tee README.md <<-'EOF'
 # 个人知识库整理
